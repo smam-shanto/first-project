043da1c598a5a26c33ad0a726dd1a19265a8b81e
smam-shanto <sshanto005@gmail,com>
2022-01-02 16:21:43 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.black,
+          body: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:<Widget> [
+                CircleAvatar(
+                  radius: 70.0,
+                  backgroundImage: AssetImage('images/main.jpg'),
+                ),
+                Text("Smam Shanto",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40.0,
+                  letterSpacing: 2.0,
+                  color: Colors.white,
+
+                ),),
+                Text("Flutter Developer",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  letterSpacing: 2.0,
+                  color: Colors.white,
+                ),),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.lightBlueAccent,
+                    ),
+
+                    title: Text(
+                      '+8801727907505',
+                      style: TextStyle(
+                        color:Colors.lightBlueAccent,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.0,
+                        fontSize: 20.0 
+                        ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.mail,
+                      color: Colors.lightBlueAccent,
+                    ),
+
+                    title: Text(
+                      'sshanto005@gmail.com',
+                      style: TextStyle(
+                        color:Colors.lightBlueAccent,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.0,
+                        fontSize: 20.0 
+                        ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.location_city,
+                      color: Colors.lightBlueAccent,
+                    ),
+                    title: Text(
+                      'west katashur, Mohammadpur, Dhaka',
+                      style: TextStyle(color: Colors.lightBlueAccent,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                      letterSpacing: 1.0
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
